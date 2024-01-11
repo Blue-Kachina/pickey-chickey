0ee073a89325adbf77dae9a5163a9b2cbbd4a922
Matthew Leering <matthew.leering@directimpactsolutions.com>
2024-01-10 19:33:59 -0500

    Now displaying image within threaded embed

--- commands/utility/checkin.js
+++ commands/utility/checkin.js
@@ -1,36 +1,53 @@
-const { SlashCommandBuilder, EmbedBuilder  } = require('discord.js');
+const { SlashCommandBuilder, EmbedBuilder, Attachment, AttachmentBuilder, ActionRowBuilder, ActionRow, ButtonBuilder, ButtonStyle} = require('discord.js');
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName('checkin')
         .setDescription('Starts a new thread to begin the checkin process'),
     async execute(interaction) {
+
         let this_channel = interaction.client.channels.cache.get(interaction.channelId)
         let author = interaction.client.users.cache.get(interaction.user?.id)
-        let embed_details = {
-            title: "Monday Night Combat",
-            description: "Initial description can go here, but it gets overwritten below anyway", // "@everyone Please react to this post if you want to join",
-            fields:[]
-        }
+
         await this_channel.threads.create({
-            name: "New MNC Event Thread",
+            name: "Event Chickin",
             reason: "To Organize"
         }).then(threadChannel => {
+
+            const attachment = new AttachmentBuilder('./assets/logo_mnc.png')
+                .setName('logo_mnc.png');
+
+            const chickin_button = new ButtonBuilder()
+                .setCustomId('chickin')
+                .setLabel('Chick me in')
+                .setStyle(ButtonStyle.Secondary);
+
+            const action_row = new ActionRowBuilder()
+                .addComponents(chickin_button);
+
             const embed = new EmbedBuilder()
                 .setColor(0x0099FF)
-                .setTitle(embed_details.title)
-                .setURL('https://discord.js.org/')
-                .setDescription(embed_details.description)
-                .setThumbnail(embed_details.thumbnail)
-                .setAuthor({name:author.username})
-                .setImage('https://i.imgur.com/AfFp7pu.png')
+                .setTitle("Who Wants Bacon?")
+                // .setURL('https://discord.js.org/')
+                .setDescription("Hiyo Everybody.  It's time to make sure that we know who's ready/available to play.  Please chickin by clucking the button below")
+                // .setThumbnail(embed_details.thumbnail)
+                .setAuthor({name:author?.username})
+                .setImage('attachment://logo_mnc.png')
                 .setTimestamp()
+                .setFields([
+                    { name: 'Player 1', value: author?.username },
+                ])
+                .setFooter({text: 'Why are chickens funny?'})
 
-            threadChannel.send({embeds: [embed]})
+            threadChannel.send({
+                embeds: [embed],
+                components: [action_row],
+                files: [attachment]
+            })
                 .then(response => {
                     // An example of editing an embed that was previously posted
-                    embed.setDescription("Hiyo Everybody, it's scrum time once again.  It's time for everyone to chickin. Please react to this post")
-                    response.edit({embeds:[embed]})
+                    embed.setDescription("Description of that same embed can now be updated like this")
+                    // response.edit({embeds:[embed]})
 
                     // ToDo: record this data in the DB so that we'll be able to use it in order to relate reactions etc...
                     let data = {
@@ -46,6 +63,7 @@ module.exports = {
                 })
 
         })
-        await interaction.reply('Pong!');
+        // await interaction.reply('Pong!');
+        await interaction.reply("Chickin Process Has Begun.  Please see details in the thread!");
     },
 };
